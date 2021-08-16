c8d00fa53943dc6e8261a0a299d3fcd1ae359edb
aa-ag <aaron.aguerrevere@gmail.com>
2021-08-16 18:21:14 -0500

    renamed scrip.py into timing.py as i'll be testing different ways to time code; starting wit time & timeit

---
+++ timing.py
@@ -0,0 +1,14 @@
+############------------ IMPORTS ------------############
+import time
+import timeit
+
+
+############------------ FUNCTION(S) ------------############
+def test_function():
+    print("Test Function Starting")
+    time.sleep(5)
+    print("5 seconds have elapsed;  function ended.")
+
+############------------ DRIVER CODE ------------############
+if __name__ == "__main__":
+    test_function()
