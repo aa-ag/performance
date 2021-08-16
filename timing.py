77a1beb306cc55d4f642521275e1eb5fd30f0548
aa-ag <aaron.aguerrevere@gmail.com>
2021-08-16 18:34:54 -0500

    refactored timing.py to not print when function starts and when it ends, but instead added three test functions, which runs the function an N number of times and prints out how many seconds it actually takes to run a function that sleeps for 5 seconds;  to my surprise, it's > than 5 seconds

--- timing.py
+++ timing.py
@@ -1,15 +1,39 @@
+############------------ DOCUMENTATION ------------############
+# Timeit utility: https://docs.python.org/3/library/timeit.html
+
+
 ############------------ IMPORTS ------------############
 import time
 import timeit
 
 
 ############------------ FUNCTION(S) ------------############
-def test_function():
-    print("Test Function Starting")
+def this_is_a_function():
     time.sleep(5)
-    print("5 seconds have elapsed;  function ended.")
+
+
+############------------ TEST(S) ------------############
+def test_function_one():
+    print( timeit.Timer(this_is_a_function).timeit(number=1) )
+
+
+def test_function_two():
+    print( timeit.Timer(this_is_a_function).timeit(number=2) )
+
+
+def test_function_three():
+    print( timeit.Timer(this_is_a_function).timeit(number=10) )
 
 ############------------ DRIVER CODE ------------############
 if __name__ == "__main__":
-    print(timeit.Timer(test_function).timeit(number=1))
-    # 5.000440147
+    test_function_one()
+    # first run
+    # 5.000404831999999
+
+    test_function_two()
+    # first run
+    # 10.005152463999998
+
+    test_function_three()
+    # first run
+    # 50.027108164
