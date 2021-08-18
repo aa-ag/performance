4bb9357c76cbbb521ebb71db1a90b98286dfe58a
aa-ag <aaron.aguerrevere@gmail.com>
2021-08-18 18:50:36 -0500

    added example from documentation

---
+++ yappi_script.py
@@ -0,0 +1,18 @@
+############------------ DOCUMENTATION ------------############
+# https://pypi.org/project/yappi/
+
+############------------ IMPORTS ------------############
+import yappi
+
+
+############------------ FUNCTION(S) ------------############
+def a():
+    for _ in range(10000000):  # do something CPU heavy
+        pass
+
+yappi.set_clock_type("cpu") # Use set_clock_type("wall") for wall time
+yappi.start()
+a()
+
+yappi.get_func_stats().print_all()
+yappi.get_thread_stats().print_all()
