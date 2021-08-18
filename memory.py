1005ad5a298ea1d600423371e33d56e7f3041910
aa-ag <aaron.aguerrevere@gmail.com>
2021-08-17 20:56:34 -0500

    added boilerplate code from documentation to test @profile decorator

--- memory.py
+++ memory.py
@@ -3,7 +3,12 @@ from memory_profiler import profile
 
 
 ############------------ FUNCTION(S) ------------############
-
+@profile
+def this_is_a_function():
+    a = [1] * (10 ** 6)
+    b = [2] * (2 * 10 ** 7)
+    del b
+    return a
 
 ############------------ DRIVER CODE ------------############
 if __name__ == "__main__":
