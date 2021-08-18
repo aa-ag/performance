376f685042d7d953b3e40193701825dbd615ea83
aa-ag <aaron.aguerrevere@gmail.com>
2021-08-18 18:54:57 -0500

    refactored to include driver code logic, tested with even larger input number

--- yappi_script.py
+++ yappi_script.py
@@ -6,22 +6,25 @@ import yappi
 
 
 ############------------ FUNCTION(S) ------------############
-def a():
-    for _ in range(10000000):  # do something CPU heavy
+def this_is_a_function():
+    for i in range(1000000000):  # do something CPU heavy
         pass
 
-yappi.set_clock_type("cpu") # Use set_clock_type("wall") for wall time
-yappi.start()
-a()
 
-yappi.get_func_stats().print_all()
-# yappi.get_thread_stats().print_all()
+############------------ DRIVER CODE ------------############
+if __name__ == "__main__":
+    yappi.set_clock_type("cpu") 
+    # Use set_clock_type("wall") for wall time
+    yappi.start()
+    this_is_a_function()
+    yappi.get_func_stats().print_all()
+    # yappi.get_thread_stats().print_all()
 
-'''
-Clock type: CPU
-Ordered by: totaltime, desc
+    '''
+    Clock type: CPU
+    Ordered by: totaltime, desc
 
-name                                  ncall  tsub      ttot      tavg      
-..ts/performance/yappi_script.py:9 a  1      0.185855  0.185855  0.185855
+    name                                  ncall  tsub      ttot      tavg      
+    ..ppi_script.py:9 this_is_a_function  1      18.41965  18.41965  18.41965
 
-'''
+    '''
